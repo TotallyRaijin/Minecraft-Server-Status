a75a3c129099488abf4eb8f44291d3090b8c3cc6
TheRealPikachu <hello@therealpikachu.net>
2021-01-20 08:36:05 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.client();
 var config = require('./config.json');
 const token = config.token
-const status = require('minecraft-server-status');
+const { getStatus } = require("mc-server-status")
 
 client.on('ready', () => {
   console.log(`I'm running!`);
 });
+
+client.on('message', msg => {
+  if (msg.content.startsWith('!status') {
+      var msgcontent = message.content.substr("!status ".length);
+      const status = await getStatus(msgcontent)
