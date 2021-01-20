ea6dcc1739b55c5118b4e7d9ecdc21571ec289fc
TheRealPikachu <hello@therealpikachu.net>
2021-01-20 08:39:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,3 @@ const { getStatus } = require("mc-server-status")
 client.on('ready', () => {
   console.log(`I'm running!`);
 });
-
-client.on('message', msg => {
-  if (msg.content.startsWith('!status') {
-      var msgcontent = message.content.substr("!status ".length);
-      const status = await getStatus(msgcontent)
